0bf62652630ac42115b033f1d6c739a5e4b094c7
michellechan0 <150152668+michellechan0@users.noreply.github.com>
2025-06-06 09:41:44 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,37 +1,21 @@
-class NilaiUlangan {
-    String nama_pelajaran = "Fisika";
-    int kelas = 11;
+class Data{
+    private int DataPriv = 20; //KeyWordPrivate
 
-    NilaiUlangan (String nama_pelajaran, int kelas){
-        this.nama_pelajaran = nama_pelajaran;
-        this.kelas = kelas;
-
-    }
-    void display(){
-        System.out.println("Nilai ujian " + nama_pelajaran + " kelas " + kelas );
-    }
-}
-
-class NilaiSiswa extends NilaiUlangan {
-    String nama_siswa;
-    int nilai;
-
-    NilaiSiswa(String nama_siswa, String nama_pelajaran, int kelas, int nilai) {
-        super(nama_pelajaran, kelas);
-        this.nama_siswa = nama_siswa;
-        this.nilai = nilai;
+    // GETTER:
+    int getDataPriv(){
+        return this.DataPriv;
     }
 
-    void display() {
-        System.out.println("Nama Siswa: " + nama_siswa);
-        super.display(); 
-        System.out.println("Nilai: " + nilai);
+    //SETTER
+    void setDataPriv(int DataPriv){
+        this.DataPriv = DataPriv;
     }
 }
-
 public class App {
-    public static void main(String[] args) {
-        NilaiSiswa siswa1 = new NilaiSiswa("jeni", "Fisika", 11, 85);
-        siswa1.display();
+    public static void main(String[] args){
+        Data D1 = new Data();
+        System.out.println("nilai ujian fisika dodi = " + D1.getDataPriv());
+
+
     }
 }
